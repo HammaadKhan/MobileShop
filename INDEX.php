5fa3155e36e23627d653a1be9d77b81695b83e6b
HammaadKhan <nogain447khan@gmail.com>
2024-05-30 16:20:50 +0500

    as

--- INDEX.php
+++ INDEX.php
@@ -3,17 +3,17 @@
  $title = "Mobile Shop";
 
 
- error_reporting(0);
- session_start();
-   include('HTML_start.php');
-   include('./Components/_Header.php');
-   include('./Components/_Hero.php');
-   include('./Components/_Center_Block.php');
-   include('./Components/_Offer_Advertisment.php');
-   include('./Components/_Featured_Products.php');
-   include ('./Components/_contact.php');
-   include('./Components/Repair_Pg.php');
-   include('./Components/_Footer.php');
-   include('./Tawak_To..php');
-   include('HTML_end.php');
+  error_reporting(0);
+  session_start();
+  include('HTML_start.php');
+  include('./Components/_Header.php');
+  include('./Components/_Hero.php');
+  include('./Components/_Center_Block.php');
+  include('./Components/_Offer_Advertisment.php');
+  include('./Components/_Featured_Products.php');
+  include ('./Components/_contact.php');
+  include('./Components/Repair_Pg.php');
+  include('./Components/_Footer.php');
+  include('./Tawak_To..php');
+  include('HTML_end.php');
 ?>
